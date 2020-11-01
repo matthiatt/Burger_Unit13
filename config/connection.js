c61ccbe53ad1654b182341123aeda01605c3c2a4
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-01 15:52:15 -0600

    edit - connection.js

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// setup the code to connect Node to MySQL.
+// Export the connection.
+const mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+
+connection.connect();
+
+module.exports = connection;
